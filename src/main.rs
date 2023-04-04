1b6ef8418f2345b53745aa7367a2eda8d2a48902
soushi888 <sacha.pignot@protonmail.com>
2023-04-04 01:20:25 -0400

    get config from toml file + monitor watch directory

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@ mod cli;
 mod daemon;
 mod monitor;
 
-fn main() {
-    println!("Hello, world!");
+// use monitor::watch_directory;
+
+use std::fs;
+use toml;
+use serde::Deserialize;
+use notify::{RecursiveMode, Watcher};
+use std::path::Path;
+use crate::monitor::watch_directory;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+  location: String,
+}
+
+fn main() -> Result<(), notify::Error> {
+  let location = get_config().location;
+  watch_directory(&location)
+}
+
+fn get_config() -> Config {
+  let config_file = fs::read_to_string("config.toml").expect("Unable to read config file");
+  toml::from_str::<Config>(&config_file).expect("Unable to parse config file")
 }
---
+++ src/monitor.rs
@@ -0,0 +1,20 @@
+use std::path::Path;
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use std::sync::mpsc;
+
+pub fn watch_directory(path: &str) -> Result<(), notify::Error> {
+  let (tx, rx) = mpsc::channel();
+
+  let mut watcher =RecommendedWatcher::new(tx, Config::default())?;
+
+  watcher.watch(Path::new(path), RecursiveMode::Recursive)?;
+
+  for res in rx {
+    match res {
+      Ok(event) => println!("changed: {:?}", event),
+      Err(e) => println!("watch error: {:?}", e),
+    }
+  }
+
+  Ok(())
+}
