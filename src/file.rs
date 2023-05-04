ea7fdc1941f77cb94ce56684ff1bdba70854098d
soushi888 <sacha.pignot@protonmail.com>
2023-05-04 03:47:40 -0400

    format with rustfmt

--- src/file.rs
+++ src/file.rs
@@ -1,131 +1,129 @@
-use std::path::Path;
+use crate::get_config;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::fs::File;
 use std::io::Write;
-use serde_json;
-use serde::{Serialize, Deserialize};
+use std::path::Path;
 use walkdir::WalkDir;
-use crate::get_config;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct FileData {
-  pub path: String,
-  pub content: String,
+    pub path: String,
+    pub content: String,
 }
 
 #[derive(Debug)]
 pub enum Error {
-  Io(std::io::Error),
-  Json(serde_json::Error),
+    Io(std::io::Error),
+    Json(serde_json::Error),
 }
 
 #[allow(dead_code)]
 impl FileData {
-  pub fn new(path: String, content: String) -> Self {
-    Self {
-      path,
-      content,
-    }
-  }
-
-  pub fn write_on_fs(&self) -> Result<(), Error> {
-    let root = get_config().location;
-    let path = Path::new(&root).join(&self.path);
-
-    if let Some(parent) = path.parent() {
-      if !parent.exists() {
-        std::fs::create_dir_all(parent).map_err(Error::Io)?;
-      }
+    pub fn new(path: String, content: String) -> Self {
+        Self { path, content }
     }
 
-    let mut file = match File::create(path) {
-      Ok(file) => file,
-      Err(err) => {
-        eprintln!("Error creating file: {}", err);
-        return Err(Error::Io(err));
-      }
-    };
+    pub fn write_on_fs(&self) -> Result<(), Error> {
+        let root = get_config().location;
+        let path = Path::new(&root).join(&self.path);
 
-    file.write_all(self.content.as_bytes()).map_err(Error::Io)
-  }
+        if let Some(parent) = path.parent() {
+            if !parent.exists() {
+                std::fs::create_dir_all(parent).map_err(Error::Io)?;
+            }
+        }
 
-  pub fn save(&self) -> Result<(), Error> {
-    let file = File::open("./files.json").map_err(Error::Io)?;
-    let mut files: Vec<FileData> = serde_json::from_reader(file).map_err(Error::Json)?;
+        let mut file = match File::create(path) {
+            Ok(file) => file,
+            Err(err) => {
+                eprintln!("Error creating file: {}", err);
+                return Err(Error::Io(err));
+            }
+        };
 
-    if let Some(index) = files.iter().position(|file| file.path == self.path) {
-      files[index] = self.clone();
-    } else {
-      files.push(self.clone());
+        file.write_all(self.content.as_bytes()).map_err(Error::Io)
     }
 
-    let file = File::create("./files.json").map_err(Error::Io)?;
-    serde_json::to_writer_pretty(file, &files).map_err(Error::Json)
-  }
+    pub fn save(&self) -> Result<(), Error> {
+        let file = File::open("./files.json").map_err(Error::Io)?;
+        let mut files: Vec<FileData> = serde_json::from_reader(file).map_err(Error::Json)?;
 
-  pub fn delete(&self) -> Result<(), Error> {
-    let file = File::open("./files.json").map_err(Error::Io)?;
-    let mut files: Vec<FileData> = serde_json::from_reader(file).map_err(Error::Json)?;
+        if let Some(index) = files.iter().position(|file| file.path == self.path) {
+            files[index] = self.clone();
+        } else {
+            files.push(self.clone());
+        }
 
-    if let Some(index) = files.iter().position(|file| file.path == self.path) {
-      files.remove(index);
+        let file = File::create("./files.json").map_err(Error::Io)?;
+        serde_json::to_writer_pretty(file, &files).map_err(Error::Json)
     }
 
-    let file = File::create("./files.json").map_err(Error::Io)?;
-    serde_json::to_writer_pretty(file, &files).map_err(Error::Json)
-  }
+    pub fn delete(&self) -> Result<(), Error> {
+        let file = File::open("./files.json").map_err(Error::Io)?;
+        let mut files: Vec<FileData> = serde_json::from_reader(file).map_err(Error::Json)?;
+
+        if let Some(index) = files.iter().position(|file| file.path == self.path) {
+            files.remove(index);
+        }
+
+        let file = File::create("./files.json").map_err(Error::Io)?;
+        serde_json::to_writer_pretty(file, &files).map_err(Error::Json)
+    }
 }
 
 pub fn get_all_files_from_mock() -> Result<Vec<FileData>, Error> {
-  let path = Path::new("./files.json");
-  let file = match File::open(path) {
-    Ok(file) => file,
-    Err(err) => {
-      eprintln!("Error opening file: {}", err);
-      return Ok(vec![]);
+    let path = Path::new("./files.json");
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("Error opening file: {}", err);
+            return Ok(vec![]);
+        }
+    };
+    let files: Vec<FileData> = serde_json::from_reader(file).map_err(Error::Json)?;
+
+    for file in files.iter() {
+        file.write_on_fs()?;
     }
-  };
-  let files: Vec<FileData> = serde_json::from_reader(file).map_err(Error::Json)?;
 
-  for file in files.iter() {
-    file.write_on_fs()?;
-  }
+    println!("Files synced from mock to FS: {:#?}", files);
 
-  println!("Files synced from mock to FS: {:#?}", files);
-
-  Ok(files)
+    Ok(files)
 }
 
 pub fn get_all_files_from_fs(path: &str) -> Vec<String> {
-  let mut file_paths = vec![];
+    let mut file_paths = vec![];
 
-  let files = WalkDir::new(path).into_iter().filter_map(Result::ok).filter(|e| e.file_type().is_file());
+    let files = WalkDir::new(path)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| e.file_type().is_file());
 
-  for entry in files {
-    let path_str = entry.path().to_str().unwrap().to_string();
-    file_paths.push(path_str);
-  }
+    for entry in files {
+        let path_str = entry.path().to_str().unwrap().to_string();
+        file_paths.push(path_str);
+    }
 
-  file_paths
+    file_paths
 }
-
 
 // Tests
-#[test]
-fn test_get_all_files() {
-  let files = get_all_files().unwrap();
-  assert_eq!(files.len(), 3);
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_write_on_fs() {
-  let file = FileData::new("test.txt".to_string(), "Hello World!".to_string());
+    #[test]
+    fn test_write_on_fs() {
+        let file = FileData::new("test.txt".to_string(), "Hello World!".to_string());
 
-  assert!(file.write_on_fs().is_ok());
+        assert!(file.write_on_fs().is_ok());
 
-  let location = get_config().location;
-  let path = Path::new(&location).join("test.txt");
+        let location = get_config().location;
+        let path = Path::new(&location).join("test.txt");
 
-  if path.exists() {
-    std::fs::remove_file(path).unwrap();
-  }
+        if path.exists() {
+            std::fs::remove_file(path).unwrap();
+        }
+    }
 }
